615fdc887a03272818e9232d46d18a716f136226
aldenhu <msmouse@gmail.com>
2024-07-24 01:02:16 +0000

    refactor: remove commit_blocks_ext
    Used to exist to not to change tests,
    while in fact most tests pass with the real thing
    (no force flushing the bufferred state),
    with the exception of some proof verification added later
    than the bufferred state change.
    
    Not being able to get state proof when state
    is still in the buffer is nonetheless an issue
    we need to address if we open APIs to the light client
    so I'm removing the hack here and defer that to
    further proper treatment. I made the flush interval
    small for tests though, to let the verification still
    happen occasionally in the integration test.

--- state-sync/state-sync-driver/src/tests/mocks.rs
+++ state-sync/state-sync-driver/src/tests/mocks.rs
@@ -290,7 +290,7 @@ mock! {
             ledger_version: Version,
         ) -> Result<TransactionAccumulatorSummary>;
 
-        fn get_state_leaf_count(&self, version: Version) -> Result<usize>;
+        fn get_state_item_count(&self, version: Version) -> Result<usize>;
 
         fn get_state_value_chunk_with_proof(
             &self,
